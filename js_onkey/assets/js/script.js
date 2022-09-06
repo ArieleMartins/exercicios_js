0156d0261fc60d2ce772e41cf684eaf56a24d432
ArieleMartins <ariele.cmmelo@gmail.com>
2022-09-06 14:51:14 -0300

    Projetos

--- js_onkey/assets/js/script.js
+++ js_onkey/assets/js/script.js
@@ -21,20 +21,3 @@ document.onkeydown = function test(event){
     }
     
 }
-document.onkeyup = function test(event){
-    if(event.key == 'ArrowUp'){
-        caixa.style.bottom = cb + 'px';
-        cb = cb + 5;
-        console.log(cb);
-    }else if(event.key == 'ArrowDown'){
-        cb = cb - 5;
-        caixa.style.bottom = cb + 'px';
-        console.log(cb);
-    }else if(event.key == "ArrowRight"){
-        caixa.style.left = er + 'px';
-        er = er + 5;
-    }else if(event.key == "ArrowLeft"){
-        er = er - 5;
-        caixa.style.left = er + 'px';
-    }
-}
