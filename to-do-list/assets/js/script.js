b00e4ef36ff75ba725675683fe2c819f26bb68b5
ArieleMartins <ariele.cmmelo@gmail.com>
2022-09-28 20:18:56 -0300

    Estilizando e adicionando funções novas

--- to-do-list/assets/js/script.js
+++ to-do-list/assets/js/script.js
@@ -4,8 +4,33 @@
 var bt = document.getElementById('ad');
 var input = document.getElementById('dados');
 var list = document.getElementById('list');
+const erro = document.getElementById('error')
+var index = [];
+var total = 0;
 bt.addEventListener('click', function adicionar() {
     var dados = input.value;
-    list.innerHTML += "<li id='item'><input type='checkbox''><label>" + dados + "</label></li>";
-    input.value = "";
+    if (!dados) {
+        erro.style.color = 'red';
+        erro.style.fontSize = '12px'
+        erro.style.textAlign = 'center'
+        erro.style.width = '100%'
+        erro.innerText = 'Por favor preencha o campo'
+    } else {
+        erro.innerText = ""
+        list.innerHTML += `<li id='${dados}'><div style='display:flex; column-gap:10px; '><input type='checkbox''><label style="display:flex;flex-wrap:wrap">${dados}</label></div><div><button  class="delete" id='${total}' value='${dados}' ><i class="fa-solid fa-trash-can"></i></button></div></li>`;
+        input.value = "";
+        index.push(total);
+        total = total + 1
+        total = index;
+    }
+    for (var i = 0; i < index.length; i++){
+        const del = document.getElementById(i)
+        del.addEventListener('click', function deletar(){
+            console.log(index);
+            const elemento = document.getElementById(del.value)
+            list.removeChild(elemento);
+            index.splice(i, 1);
+            console.log(index)
+        })
+    }
 })
