949a08638f010a751e9528b1b09b5512096657a8
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-17 19:14:26 -0300

    Update Js clean code

--- jogo_adivinhar/assets/js/script.js
+++ jogo_adivinhar/assets/js/script.js
@@ -1,38 +1,111 @@
-var input = document.getElementById('dados');
-var enviar = document.getElementById('enviar');
-var pontuacao = document.getElementById('adivinho');
-var pontos = 0;
-var tentativa = 0;
-var tentativas = document.getElementById('tentativas');
-var number = Math.floor(Math.random)
+const btnPlay = document.getElementById('btnPlay')
+const modal = document.querySelector('.modal')
+const inputNumberEnd = document.getElementById('numberEnd')
+const inputNumberAttempts = document.getElementById('numberAttempts')
+const submitNumberPlayer = document.getElementById('submitNumberPlayer')
+const numberPlayer = document.getElementById('numberPlayer')
+const guessed = document.getElementById('guessed')
+const spanWinner = document.querySelector('.winner')
+const error = document.querySelector('.error')
+const containerAttempts = document.querySelector('.attempts')
+var attempts = 1
+var numberBot = 0
+var winners = 0
 
-enviar.onclick = function veriNumero(){
-    if(tentativa >= 10){
-        window.alert('Desclpa :( Você gastou todas as 10 tentativas tente novamente');
-        numero = numero = Math.floor(Math.random() * 101);
-        tentativa = 0;
-        tentativas.innerHTML = "";
-    }else{
-    if(input.value == ''){
-        window.alert('Por Favor Digite Um Número');
-    }else{
-        var n = parseInt(input.value);
-        if(n < numero){
-            tentativas.innerHTML += "<p>O número " + n + " é menor";
-            tentativa = tentativa + 1;
-        }else if(n > numero){
-            tentativas.innerHTML += "<p>O número " + n + " é maior";
-            tentativa = tentativa + 1;
-        }else if(n == numero){
-            window.alert("Parabéns você conseguiu adivinhar o número");
-            tentativas.innerHTML = "";
-            numero = Math.floor(Math.random() * 101);
-            pontos = pontos + 1;
-            pontuacao.innerHTML = "Você conseguiu adivinha: " + pontos + " números";
-            tentativa = 0;
+document.addEventListener('keydown', (event)=>{
+    if(event.key == 'Enter'){
+        const styleModal = window.getComputedStyle(modal, null).getPropertyValue('visibility')
+        if(styleModal == 'visible'){
+            play()
+        }else{
+            checkNumberPlaye()
         }
-        
+    }
+})
+
+inputNumberEnd.addEventListener('input', (event)=>{
+    const checkData = isNaN(Number(event.data))
+    checkDataInputs(checkData, inputNumberEnd)
+})
+
+inputNumberAttempts.addEventListener('input', (event) =>{
+    const checkData = isNaN(Number(event.data))
+    checkDataInputs(checkData, inputNumberAttempts)
+})
+
+numberPlayer.addEventListener('input', (event) =>{
+    const checkData = isNaN(Number(event.data))
+    checkDataInputs(checkData, numberPlayer)
+})
+
+btnPlay.addEventListener('click', () =>{
+    play()
+})
+
+submitNumberPlayer.addEventListener('click', ()=>{
+    checkNumberPlaye()
+})
+
+function checkDataInputs(data, input){
+    if(data){
+        input.value = ''
     }
 }
-    
+
+function play(){
+    modal.style.visibility = "hidden"
+    numberBot = moveBot(Number(inputNumberEnd.value))
+    numberPlayer.setAttribute('max', inputNumberEnd.value)
+}
+
+function moveBot(numberEnd){
+    var number = Math.floor(Math.random() * (numberEnd + 1))
+    return number
+}
+
+function checkNumberPlaye(){
+    if(attempts <= Number(inputNumberAttempts.value)){
+
+        const numberPlayerValue = Number(numberPlayer.value)
+
+        if(numberPlayerValue > Number(inputNumberEnd.value) || numberPlayerValue < 0){
+            error.style.color = 'red'
+            error.innerText = `Somente números entre 0 e ${inputNumberEnd.value}`
+        }else{
+            error.innerText = ''
+
+            const checkWinner = numberPlayerValue == numberBot ? true : false
+
+            if(checkWinner){
+                winnerPlayer()
+            }else{
+                if(numberPlayerValue > numberBot){
+                    containerAttempts.innerHTML += `<span>O número ${numberPlayerValue} é maior</span>`
+                }else if (numberPlayerValue < numberBot){
+                    containerAttempts.innerHTML += `<span>O número ${numberPlayerValue} é menor</span>`
+                }
+                attempts += 1
+            }
+        }
+        
+
+    }else{
+
+        finishedAttempts()
+
+    }
+}
+
+function winnerPlayer(){
+    spanWinner.innerHTML = "Parabéns você consegui acerta o numero"
+    winners += 1
+    guessed.innerText = winners
+    btnPlay.innerText = "Reset"
+    modal.style.visibility = "visible"
+}
+
+function finishedAttempts(){
+    spanWinner.innerHTML = "Suas tentativas acabaram 🙁"
+    btnPlay.innerText = "Reset"
+    modal.style.visibility = "visible"
 }
