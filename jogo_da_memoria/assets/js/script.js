e9909f6d67f75dcc5bca055260de0e1a2c379231
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-22 14:37:21 -0300

    Update

--- jogo_da_memoria/assets/js/script.js
+++ jogo_da_memoria/assets/js/script.js
@@ -297,13 +297,16 @@ function visibleModalFail(){
 async function checkTheme(){ // verificar o tema selecionado pelo usuário
     const radioThemePokemon = document.getElementById("pokemon")
     const radioThemeRickMorty = document.getElementById("rickmorty")
+    const subtitleTheme = document.querySelector('.theme')
 
     if(radioThemePokemon.checked){
         theme = false
+        subtitleTheme.innerText = "Pokémon"
         var url = 'https://pokeapi.co/api/v2/pokemon?limit=50'
         return url
     }else if(radioThemeRickMorty.checked){
         theme = true
+        subtitleTheme.innerText = "Rick and Morty"
         var url = 'https://rickandmortyapi.com/api/character/?page=2'
         return url
     }
@@ -368,6 +371,7 @@ checkboxTimer.addEventListener("click", ()=>{
     if(checkboxTimer.checked){
         spanRangeTimer.style.visibility = 'visible'
         rangeTimer.style.visibility = 'visible'
+        
     }else{
         spanRangeTimer.style.visibility = 'hidden'
         rangeTimer.style.visibility = 'hidden'
