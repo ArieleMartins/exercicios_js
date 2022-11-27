fd35ac10d56bd9368291137013a2a77ebac1576d
ArieleMartins <ariele.cmmelo@gmail.com>
2022-11-27 18:50:08 -0300

    Adjust style and clean code

--- who_is_that_pokemon/assets/js/modules/api.js
+++ who_is_that_pokemon/assets/js/modules/api.js
@@ -1,13 +1,13 @@
-export async function acessUrl(number){
+export async function acessUrl(){
     try{
-        const url = `https://pokeapi.co/api/v2/pokemon/${number}/`
+        var url = `https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 899)}/`
         const json = await fetch(url).then(response => response.json())
         const img = await json.sprites.front_default
         const name = await json.name
         return {
             img,
             name
-        }
+        } 
     }catch (error){
         console.log("Opa.. deu erro " + error)
     }
--- who_is_that_pokemon/assets/js/script.js
+++ who_is_that_pokemon/assets/js/script.js
@@ -1,7 +1,6 @@
 import { acessUrl as api } from './modules/api.js'
-let number = Math.floor(Math.random() * 200)
 
-const object = await api(number)
+const object = await api()
 
 const img = document.getElementById('img-pokemon')
 
